fd91d580a117ccee28ce9cc49e5a4520407a7f05
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-07-15 10:13:43 -0300

    Ajuste no projeto

--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,13 @@
  -->
   <div class="w-1/2 mx-auto my-20">
 
-  <button class="bg-red-500 p-3 text-white" v-on:click="carregarLista()">
+  <button class="bg-red-500 p-3 text-white" v-on:click="carregarListaUmItem()">
     ReloadArticles
   </button>
 
     <h1 class="text-3xl font-semibold text-blue-500">{{tituloBlog}}</h1>
 
-    <article v-for="article in listaArtigos" v-bind:key="article" class="my-10 text-gray-600">
+    <article v-for="article in listaArtigos" v-bind:key="article.id" class="my-10 text-gray-600">
       <h2 class="text-5xl font-semibold mb-5">{{article.title}}</h2>
       <p class="mb-5">{{article.text}}</p>
       <p class="text-blue-700">Ler mais</p>
@@ -27,8 +27,6 @@
 </template>
 
 <script>
-
-import axios from 'axios';
 
 
   export default {
@@ -45,28 +43,36 @@ import axios from 'axios';
     },
 
     methods: {
+      carregarListaUmItem() {
+        this.listaArtigos = [
+          {
+            "id": 1,
+            "title":"Google é multado em R$ 3 bi por não negociar pagamento de notícia na França",
+            "text": "Todos os países da UE tinham o prazo até o meio de 2021 para se adequar. O Google afirmou que iria pagar para que notícias selecionadas de ..."
+          }
+        ]
+      },
       carregarLista() {
-          this.listaArtigos = [
-            {
-              "title":"Google é multado em R$ 3 bi por não negociar pagamento de notícia na França",
-              "text": "Todos os países da UE tinham o prazo até o meio de 2021 para se adequar. O Google afirmou que iria pagar para que notícias selecionadas de ..."
-            },
-            {
-              "title": "Serviço do Google separa joio do trigo para leitores de ...",
-              "text": "Serviço do Google separa joio do trigo para leitores de notícias. Feito por jornalistas profissionais, o Destaques já tem mais de 45 veículos de ..."
-            },
-            {
-              "title": "Google Camera 8.3 é lançada com novo design baseado no ...",
-              "text": "Dessa forma, se você quiser testar a nova versão do aplicativo, terá que instalar o novo Android 12 Beta 3 em um celular Pixel do Google, pois o ..."
-            }
+        this.listaArtigos = [
+          {
+            "id": 1,
+            "title":"Google é multado em R$ 3 bi por não negociar pagamento de notícia na França",
+            "text": "Todos os países da UE tinham o prazo até o meio de 2021 para se adequar. O Google afirmou que iria pagar para que notícias selecionadas de ..."
+          },
+          {
+            "id": 2,
+            "title": "Serviço do Google separa joio do trigo para leitores de ...",
+            "text": "Serviço do Google separa joio do trigo para leitores de notícias. Feito por jornalistas profissionais, o Destaques já tem mais de 45 veículos de ..."
+          },
+          {
+            "id": 3,
+            "title": "Google Camera 8.3 é lançada com novo design baseado no ...",
+            "text": "Dessa forma, se você quiser testar a nova versão do aplicativo, terá que instalar o novo Android 12 Beta 3 em um celular Pixel do Google, pois o ..."
+          }
         ]
       }
-    }
+  }
 
   }
 </script>
 
-
-<style>
-
-</style>
